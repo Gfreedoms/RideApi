9f8af8afca9814568fcd8301ae7060d585c6609f
byarustev <byarus45@gmail.com>
2018-08-22 08:09:11 +0300

    update create_tables method to reduce its height.

--- api/database/database_handler.py
+++ api/database/database_handler.py
@@ -2,40 +2,7 @@ import psycopg2
 import psycopg2.extras as extra
 from flask import current_app
 
-
-class DataBaseConnection:
-    def __init__(self, database=None):
-            try:
-                if database:
-                    self.connection = psycopg2.connect(database=database, user="mczhkqyutgspud",
-                                                       password="bc4506c9ca483df2d1756d8fecebdc9" +
-                                                                "095df6593414ac3e85b7962f019252600",
-                                                       host="ec2-23-23-248-192.compute-1.amazonaws.com",
-                                                       port="5432")
-                else:
-                    if current_app.config["TESTING"]:
-                        self.connection = psycopg2.connect(database=current_app.config["TEST_DATABASE"], user="postgres",
-                                                           password="",
-                                                           host="localhost",
-                                                           port="5432")
-                    else:
-
-                        self.connection = psycopg2.connect(database=current_app.config["DATABASE"], user="mczhkqyutgspud",
-                                                           password="bc4506c9ca483df2d1756d8fecebdc9" +
-                                                                    "095df6593414ac3e85b7962f019252600",
-                                                           host="ec2-23-23-248-192.compute-1.amazonaws.com",
-                                                           port="5432")
-
-                self.connection.autocommit = True
-                self.cursor = self.connection.cursor()
-                self.dict_cursor = self.connection.cursor(cursor_factory=extra.DictCursor)
-
-                self.create_tables()
-            except Exception as exp:
-                print(exp)
-
-    def create_tables(self):
-        queries = (
+TABLE_QUERIES = (
             """
             CREATE TABLE IF NOT EXISTS users (
                 user_id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL,
@@ -74,15 +41,49 @@ class DataBaseConnection:
                 message text, FOREIGN KEY (user_id) REFERENCES users (user_id)
                 ON UPDATE CASCADE ON DELETE CASCADE
             )
-            """ )
+            """)
 
-        for query in queries:
+
+class DataBaseConnection:
+    def __init__(self, database=None):
+            try:
+                if database:
+                    self.connection = psycopg2.connect(database=database, user="mczhkqyutgspud",
+                                                       password="bc4506c9ca483df2d1756d8fecebdc9" +
+                                                                "095df6593414ac3e85b7962f019252600",
+                                                       host="ec2-23-23-248-192.compute-1.amazonaws.com",
+                                                       port="5432")
+                else:
+                    if current_app.config["TESTING"]:
+                        self.connection = psycopg2.connect(database=current_app.config["TEST_DATABASE"], user="postgres",
+                                                           password="",
+                                                           host="localhost",
+                                                           port="5432")
+                    else:
+
+                        self.connection = psycopg2.connect(database=current_app.config["DATABASE"], user="mczhkqyutgspud",
+                                                           password="bc4506c9ca483df2d1756d8fecebdc9" +
+                                                                    "095df6593414ac3e85b7962f019252600",
+                                                           host="ec2-23-23-248-192.compute-1.amazonaws.com",
+                                                           port="5432")
+
+                self.connection.autocommit = True
+                self.cursor = self.connection.cursor()
+                self.dict_cursor = self.connection.cursor(cursor_factory=extra.DictCursor)
+
+                self.create_tables()
+            except Exception as exp:
+                print(exp)
+
+    def create_tables(self):
+
+        for query in TABLE_QUERIES:
             self.cursor.execute(query)
 
     def drop_test_tables(self):
 
-        query="DROP TABLE IF EXISTS {0} CASCADE"
-        tables = ["users","rides","ride_requests","friend_requests","notifications"]
+        query = "DROP TABLE IF EXISTS {0} CASCADE"
+        tables = ["users", "rides", "ride_requests", "friend_requests", "notifications"]
 
         for table in tables:
             self.cursor.execute(query.format(table))
